94402140d8e5ba9555177179a023bc6935485a62
jyothihr92 <fnu.jyothihassanramesh@sjsu.edu>
2020-03-16 23:02:53 -0700

    Authorize and  UnAuthorize space is removed with div

--- webcam.js
+++ webcam.js
@@ -1,15 +1,15 @@
  
-var myParam = '';
+var myParam = 'sulalitha01@gmail.com';
 var emailId = '';
 window.onload = function() {
-                        var urlParams = new this.URLSearchParams(Window.location.search);
+                        var urlParams = new URLSearchParams(Window.location.search);
                         myParam = urlParams.get('param1');
 }
 
 var  imageList =[];
 var images='';
-var folderBucketName = 'rewardsprgmcustimages';
-var bucketRegion = 'us-east-1';
+var folderBucketName = 'storewebcamimg';
+var bucketRegion = 'us-east-2';
 var final;
 var newList =[];
 //update config
@@ -94,3 +94,128 @@ function addFile(fileToUpload) {
         return new Blob([ia], {type:mimeString});
     }
 
+//function called on click of authenticate
+function auth(){
+	var params1= {
+  Bucket: "rewardsprgmcustimages"
+ };
+ s3.listObjects(params1, function(err, data) {
+   if (err) console.log(err, err.stack); // an error occurred
+   else     {
+   var t = JSON.stringify(data.Contents[0].Key); 
+   console.log("data = " + t);
+	var imgs = data.Contents.map(function(res){
+		 var fileKey = res.Key;
+		var lastchar = fileKey.charAt(fileKey.length-1)
+		 if(lastchar != '/' ) {
+			images+=fileKey +','
+            imageList.push(fileKey);
+		 }
+    });
+    
+    var newStr;
+    newstr  = images.slice(0, -1);
+	
+  authenticate(newstr);
+	 }
+   });
+   
+   
+}
+
+//function to do the facial comparison
+function authenticate(newstr){
+    var res = newstr.split(',');
+    var match;
+    
+      
+       for(var i=0;i<res.length;i++){
+       console.log('res[i]...'+res[i]);
+       const client = new AWS.Rekognition();
+      
+       const params = {
+        SimilarityThreshold: 95,
+         SourceImage: {
+           S3Object: {
+             Bucket: "storewebcamimg",
+             Name: myParam+".jpg"
+           },
+         },
+         TargetImage: {
+           S3Object: {
+             Bucket: "rewardsprgmcustimages",
+             Name: res[i]
+           },
+         }
+       }
+        client.compareFaces(params, function(err, response) {
+         if (err) {
+        console.log( "params =  " + JSON.stringify(params));
+           console.log(err, err.stack); // an error occurred
+         } else {
+           console.log("response" + response.data)
+           console.log(response)
+          
+         console.log(response.FaceMatches);
+           response.FaceMatches.forEach(data => {
+             let position   = data.Face.BoundingBox
+             let similarity = data.Similarity
+            
+             console.log(`The face at: ${position.Left}, ${position.Top} matches with ${similarity} % confidence`)
+            match =true;
+            console.log(match);
+           }) // for response.faceDetails
+           if(response.FaceMatches.length == 0) {
+           match=false;
+           }
+           newList.push(match);
+           console.log(newList.length);
+           console.log(res.length);
+           if(newList.length == res.length) {
+           
+                var temp = newList.includes(true);
+                if(temp){
+                    successFunction();
+                }	
+                 else{
+                errorFunction();
+           }
+           }
+          
+          //final = match;
+         // return match;
+         } 
+       });
+        
+       }
+    
+      }
+      function successFunction(){
+        console.log("success")
+        document.getElementById("app").style="display:none;";
+        document.getElementById("my_camera").style = "display:none";
+        document.getElementById("authBtn").style = "display:none";
+        document.getElementById("success").style="display:block;";
+       }
+      function errorFunction(){
+      //alert(JSON.stringify(TargetImage));
+        document.getElementById("app").style="display:none;";
+        document.getElementById("error").style="display:block;";
+        
+        var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
+        var params = {
+      UserPoolId: 'us-east-2_Ah2t3hSsZ', /* required */
+      Username: myParam /* required */
+    };
+    cognitoidentityserviceprovider.adminGetUser(params, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else    { console.log(JSON.stringify(data.UserAttributes[2].Value)); 
+        emailId = data.UserAttributes[2].Value;
+        sendEmail(emailId);
+        
+    }
+      // successful response
+    });
+        console.log(emailId);
+        
+      }
